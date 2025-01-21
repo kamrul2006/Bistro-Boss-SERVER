b43652f0adebd072b3349d84d9114992f968d41c
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2025-01-22 00:59:23 +0600

    Payment with stripe added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 const port = process.env.PORT || 5000
 
 const app = express();
@@ -295,6 +296,26 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query);
             res.send(result)
+        })
+
+        //-------------------------------------------------------------------------
+        //-------Payment intent---
+        //--------------------------------------------------------------------------
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body
+            const amount = parseInt(price * 100);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+
         })
 
         await client.db("admin").command({ ping: 1 });
